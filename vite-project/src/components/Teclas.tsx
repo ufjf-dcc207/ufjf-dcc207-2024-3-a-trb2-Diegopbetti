38597c0de9ce41a061a300216994d34baab5c380
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-05 16:34:31 -0300

    Adiciona estilizacao das teclas

--- vite-project/src/components/Teclas.tsx
+++ vite-project/src/components/Teclas.tsx
@@ -2,6 +2,37 @@ import './Teclas.css'
 
 export default function(){
     return (
-        <div>teste</div>
+<div className='fileira'>
+                <div className='teclas'>Q</div>
+                <div className='teclas'>W</div>
+                <div className='teclas'>E</div>
+                <div className='teclas'>R</div>
+                <div className='teclas'>T</div>        
+                <div className='teclas'>Y</div>
+                <div className='teclas'>U</div>
+                <div className='teclas'>I</div>
+                <div className='teclas'>O</div>
+                <div className='teclas'>P</div>
+           
+                <div className='teclas'>A</div>
+                <div className='teclas'>S</div>
+                <div className='teclas'>D</div>
+                <div className='teclas'>F</div>
+                <div className='teclas'>G</div>
+                <div className='teclas'>H</div>
+                <div className='teclas'>J</div>
+                <div className='teclas'>K</div>
+                <div className='teclas'>L</div>
+                <div className='teclas'>🔙</div>
+
+                <div className='teclas'>Z</div>        
+                <div className='teclas'>X</div>
+                <div className='teclas'>C</div>
+                <div className='teclas'>V</div>
+                <div className='teclas'>B</div>
+                <div className='teclas'>N</div>      
+                <div className='teclas'>M</div>
+                <div className='teclas'>☑️</div>         
+        </div>
     )
 }
