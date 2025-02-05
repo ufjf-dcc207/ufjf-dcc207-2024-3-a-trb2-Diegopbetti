b590530afdfca6238be989887c8728ae1fbb4121
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-05 14:16:48 -0300

    Adiciona as linhas com os quadrados que irao ocupar cada letra do jogo e altera a estilizacao de alguns componentes

--- vite-project/src/components/Jogo.tsx
+++ vite-project/src/components/Jogo.tsx
@@ -1,11 +1,14 @@
 import './Jogo.css'
+import Palavra from './Palavra'
 
 function Jogo(){
 
     return (
         <div className='container'>
             <h1 className='titulo'>Termo</h1>
-            <div className='jogo'></div>
+            <div className='jogo'>
+                <Palavra />
+            </div>
             <div className='teclado'></div>
         </div>
     )
---
+++ vite-project/src/components/Palavra.tsx
@@ -0,0 +1,45 @@
+import './Palavra.css'
+
+export default function(){
+    return (
+        <div className='linha'>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+           
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+           
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+            
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+           
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+           
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+                <div className='letra'></div>
+            
+        </div>
+        
+    )
+}
