6e72533b0e838c4f16c33bea59a69f226f8614eb
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-05 22:36:54 -0300

    Importa os hooks de useState e useEffect e adiciona as constantes para mostrar a quantidade de tentativas e a quantidade de letras

--- vite-project/src/components/Jogo.tsx
+++ vite-project/src/components/Jogo.tsx
@@ -1,6 +1,10 @@
 import './Jogo.css'
 import Palavra from './Palavra'
 import Teclas from './Teclas'
+import { useState, useEffect } from 'react';
+
+const MAX_ATTEMPTS = 6; 
+const WORD_LENGTH = 5;
 
 function Jogo(){
 
