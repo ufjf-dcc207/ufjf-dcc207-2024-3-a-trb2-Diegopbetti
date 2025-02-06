9b100041b8615279daebbd7506559a454a6056e5
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-06 20:37:10 -0300

    Para cada tecla, utiliza uma div para chamar or onKeyPress ao ser clicado

--- vite-project/src/components/Teclas.tsx
+++ vite-project/src/components/Teclas.tsx
@@ -12,37 +12,12 @@ export default function Teclas({onKeyPress}: TeclasProps){
       ];
 
     return (
-            <div className='fileira'>
-                <div className='teclas'>Q</div>
-                <div className='teclas'>W</div>
-                <div className='teclas'>E</div>
-                <div className='teclas'>R</div>
-                <div className='teclas'>T</div>        
-                <div className='teclas'>Y</div>
-                <div className='teclas'>U</div>
-                <div className='teclas'>I</div>
-                <div className='teclas'>O</div>
-                <div className='teclas'>P</div>
-           
-                <div className='teclas'>A</div>
-                <div className='teclas'>S</div>
-                <div className='teclas'>D</div>
-                <div className='teclas'>F</div>
-                <div className='teclas'>G</div>
-                <div className='teclas'>H</div>
-                <div className='teclas'>J</div>
-                <div className='teclas'>K</div>
-                <div className='teclas'>L</div>
-                <div className='teclas'>🔙</div>
-
-                <div className='teclas'>Z</div>        
-                <div className='teclas'>X</div>
-                <div className='teclas'>C</div>
-                <div className='teclas'>V</div>
-                <div className='teclas'>B</div>
-                <div className='teclas'>N</div>      
-                <div className='teclas'>M</div>
-                <div className='teclas'>☑️</div>         
+        <div className='fileira'>
+            {keys.map((key) => (
+                <div className='teclas' key={key} onClick={() => onKeyPress(key)}>
+                    {key}
+                </div>
+            ))}                     
         </div>
-    )
+    );
 }
