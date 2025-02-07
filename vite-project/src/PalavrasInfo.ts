a3dda3719932125efa5c88b2fd71749129ed0052
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-07 08:06:46 -0300

    Adiciona um arquivo ts que vai conter as palavras e vai exportar uma delas de maneira aleatoria

---
+++ vite-project/src/PalavrasInfo.ts
@@ -0,0 +1,15 @@
+export const palavrasValidas = [
+    "TREVO", "PRAZO", "FELIZ", "GRATO", "MUNDO",
+    "PLANO", "BRAVO", "CLARO", "DENTE", "FALTA",
+    "FUGIR", "GENRO", "HUMOR", "JUNTA", "LIVRO",
+    "MELAO", "NOTAR", "OPACO", "PRIMO", "BICHO",
+    "ROSTO", "SALTO", "TONAL", "URANO", "VALOR",
+    "XISTO", "ZONAL", "VIRAR", "SORGO", "TURNO",
+    "COVIL", "BROTA", "DIGNO", "EIXOS", "GOSTO",
+    "HASTE", "INFRA", "JOVEM", "KARMA", "LARGO",
+    "MISTO", "NEVOA", "ORDEM", "PLUMA", "QUEDA",
+    "RAMPA", "SOPRO", "TURMA", "BRUXO", "VEXAR"
+];
+  
+export const palavraCorreta = palavrasValidas[Math.floor(Math.random() * palavrasValidas.length)];
+  
